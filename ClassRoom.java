e1ca4442a1128519133f3e711d88a3afff271fd4
Thomas Griesmayer <griesmayer@spengergasse.at>
2024-09-26 11:49:24 +0200

    Initial sharing of project

---
+++ ClassRoom.java
@@ -0,0 +1,36 @@
+public class ClassRoom
+{
+    private String  roomNr;
+    private boolean computer;
+    private int     size;
+    
+    public void setRoomNr(String newRoomNr)
+    {
+        roomNr = newRoomNr;
+    }
+    
+    public void setComputer(boolean newComputer)
+    {
+        computer = newComputer;
+    }
+    
+    public void setSize(int newSize)
+    {
+        size = newSize;
+    }
+    
+    public String getRoomNr()
+    {
+        return roomNr;
+    }
+    
+    public boolean getComputer()
+    {
+        return computer;
+    }
+    
+    public int getSize()
+    {
+        return size;
+    }    
+}
